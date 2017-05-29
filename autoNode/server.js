870d0e534f82f0ca22e7578a3ac4d16245bf6f28
caih <27247758@qq.com>
2017-05-29 10:53:51 +0800

    add info

--- autoNode/server.js
+++ autoNode/server.js
@@ -1,14 +1,15 @@
 const execFile = require('child_process').execFile;
+const exec = require('child_process').exec;
 const http = require('http');
 
 http.createServer(function(req, res) {
   res.writeHeader(200, {
     'content-Type': 'text/plain;charset=utf-8'
   });
-  
-  setInterval(autoPushGit,1000*60)
+  runCmd()
+  //setInterval(autoPushGit, 1000 * 60)
   autoPushGit();
-  
+
   res.end("执行脚本程序启动成功")
 
 }).listen(88);
@@ -25,4 +26,19 @@ function autoPushGit() {
     console.log('执行返回:' + stdout);
 
   });
+}
+
+function runCmd() {
+  //需要执行的命令字符串
+  var cli = 'ipconfig systeminfo';
+  exec(cli, {
+    encoding: 'utf8'
+  }, function(err, stdout, stderr) {
+    if(err) {
+      console.log(err);
+      return;
+    }
+    console.log('stdout' + stdout);
+    console.log('stderr' + stderr);
+  })
 }
